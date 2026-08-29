cc232e0a9f2a0399d02fb6d170fc88144ca68907
Bomi87 <cucumy@naver.com>
2026-08-29 23:54:34 +0900

    Update common-auth.js

--- common-auth.js
+++ common-auth.js
@@ -159,6 +159,7 @@ const PORTAL_APP_RULES = [
   { id: "faa-artcc-advisory-map", path: "/FAA_ATCSCC/" },
   { id: "faa-cnd-diagram", path: "/U.S_Airport_Diagram/" },
   { id: "receipt-expense-manager", path: "/Receipt/" },
+  { id: "sgbac-reservation", path: "/SGBAC/" },
 
   // 동일 경로를 쿼리스트링으로 구분
   {
